e54b930c4e791a2375297f2fc47f52a0f9658661
Micha <michael.stachurski@digitalcareerinstitute.org>
2021-08-11 13:14:07 +0200

    irgendwas

---
+++ src/components/TodoContainer/TodoContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class TodoContainer extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hi, ich bin der TodoContainer</h1>
+        <p>Ich bin eine React Component! </p>
+      </div>
+    );
+  }
+
+}
+
+export default TodoContainer; 
